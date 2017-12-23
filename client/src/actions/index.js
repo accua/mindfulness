b6b5c8e02e08091d1de815740e14395f66149c75
Andrew <andrewaccuardi@gmail.com>
2017-12-22 16:54:34 -0800

    add show action

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,15 +1,25 @@
 import axios from 'axios';
 
-const URL ='/api/entries';
+const ROOT_URL ='/api';
 
 export const FETCH_ENTRIES = 'FETCH_ENTRIES'
+export const FETCH_ENTRY = 'FETCH_ENTRY'
 
 export function fetchEntries() {
   // setting up the promise
-  const request = axios.get(URL);
+  const request = axios.get(`${ROOT_URL}/entries`);
 
   return {
     type: FETCH_ENTRIES,
+    payload: request
+  };
+}
+
+export function fetchEntry(id) {
+  const request = axios.get(`${ROOT_URL}/entries/${id}`);
+
+  return {
+    type: FETCH_ENTRY,
     payload: request
   };
 }
