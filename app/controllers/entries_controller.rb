dc44db8785d7a0348e7d890a641fdb98e55c58e1
Andrew <andrewaccuardi@gmail.com>
2018-02-03 20:04:05 -0800

    setup authenticate_user method on controllers

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,4 +1,5 @@
-class EntriesController < ApiController
+class EntriesController < ApplicationController
+  before_action :authenticate_user
   before_action :set_entry, only: [:show, :update, :destroy]
 
   # GET /entries
