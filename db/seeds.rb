a1b1c1963482f41f23d8820c02459cd8ead09910
Andrew <andrewaccuardi@gmail.com>
2018-02-03 20:05:11 -0800

    add user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,3 +13,15 @@
 
   en.feelings.create!(textbox: 'Gluten-free schlitz normcore flexitarian lyft palo santo polaroid viral four loko snackwave cray. Kickstarter crucifix cloud bread hell of four loko, food truck copper mug ugh hot chicken letterpress adaptogen air plant neutra pinterest. Affogato fashion axe echo park normcore listicle helvetica. Readymade offal tacos, raw denim pabst wayfarers seitan kitsch tofu iPhone meggings YOLO art party.', before_or_after: 1, rating: 5, created_at: Time.now+10*60)
 end
+
+user = User.new
+user.email = 'user@banaynays.com'
+user.password = 'bananaKing'
+user.password_confirmation = 'bananaKing'
+user.save
+
+user = User.new
+user.email = 'user@bananas.com'
+user.password = 'bananaBro'
+user.password_confirmation = 'bananaBro'
+user.save
